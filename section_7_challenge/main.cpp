a4acbea58b1b91e75f45f9c3d884742fb5bbc043
norbyscook <andyqe@gmail.com>
2021-02-01 20:45:18 -0800

    added first two vectors

--- section_7_challenge/main.cpp
+++ section_7_challenge/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
 int main ()
 {
+    vector <int> vector1;
+    vector <int> vector2;
+
+    vector1.push_back(10);
+    vector1.push_back(100);
+
+    vector2.push_back(200);
+    vector2.push_back(300);
+
+}
+
+void print_1d_vector(vector <int> vect)
+{
+    for(auto i = vect.begin(); i != vect.end(); i++)
+    {
         
+    }
 }
