8e0c84df9c0cc97f2c41114c48c145b5cf7bbe91
norbyscook <andyqe@gmail.com>
2021-02-01 20:54:24 -0800

    added print for 1d vector

--- section_7_challenge/main.cpp
+++ section_7_challenge/main.cpp
@@ -6,6 +6,8 @@ using std::cin;
 using std::endl;
 using std::vector;
 
+void print_1d_vector(vector <int> vect);
+
 int main ()
 {
     vector <int> vector1;
@@ -13,7 +15,8 @@ int main ()
 
     vector1.push_back(10);
     vector1.push_back(100);
-
+    print_1d_vector(vector1);
+    
     vector2.push_back(200);
     vector2.push_back(300);
 
@@ -23,6 +26,6 @@ void print_1d_vector(vector <int> vect)
 {
     for(auto i = vect.begin(); i != vect.end(); i++)
     {
-        
+        cout << *i << endl;
     }
 }
